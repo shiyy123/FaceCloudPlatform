9ea10155718c2ee60632646ad347eadd4024373d
neilfvhv <neilfvhv@gmail.com>
2016-12-02 02:11:59 +0800

    Add FaceDetection controller and view commit

---
+++ FaceCloudPlatform/FaceCloudPlatform/Controllers/FaceDetectionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FaceCloudPlatform.Controllers
+{
+    public class FaceDetectionController : Controller
+    {
+        // GET: FaceDetection
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
